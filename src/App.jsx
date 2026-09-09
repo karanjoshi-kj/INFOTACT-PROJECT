4040af45e8ff5da745b2c063eefd558ff96e39fb
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2026-09-09 13:23:26 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hello from './components/Hello';
+
 const App = () => {
   return (
     <div>
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,9 @@
+import React from 'react';
+
 function Hello() {
-  return <h1>Hello</h1>;
+  return (
+    <h1>hello</h1>
+  );
 }
 
 export default Hello;
